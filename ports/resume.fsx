7ce73e57e3e5dcd322f6dd4c6a9c9b8e1dfeb749
jarcane <jarcane@users.noreply.github.com>
2015-01-04 21:50:55 +0200

    Beginning work on F# port

---
+++ ports/resume.fsx
@@ -0,0 +1,26 @@
+﻿// resume.fsx
+// A port of resume.hsy to F#
+// Copyright 2014 John Berry
+
+// Setup path variables
+System.IO.Directory.SetCurrentDirectory(__SOURCE_DIRECTORY__)
+let datapath = "../data/"
+
+// Helper Functions
+let makeListFromFile fname =
+    Array.toList (System.IO.File.ReadAllLines(datapath + fname))
+
+let last x =
+    x |> List.rev |> List.head
+
+// Read data
+let rawDetails = makeListFromFile "details.txt"
+let rawLanguages = makeListFromFile "languages.txt"
+let rawProjects = makeListFromFile "projects.txt"
+let rawExperiences = makeListFromFile "experiences.txt"
+
+let github = rawDetails
+             |> last
+             |> (fun x -> x.Split[|'/'|])
+             |> Array.toList
+             |> last
